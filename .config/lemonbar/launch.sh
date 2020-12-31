170e73751241ab51136a6289b7ddf3dd15a75ca8
josemapt <73686197+josemapt@users.noreply.github.com>
2020-12-31 16:03:18 +0100

    Update launch.sh

--- .config/lemonbar/launch.sh
+++ .config/lemonbar/launch.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-~/.config/lemonbar/modules.sh | lemonbar -p \
-    -g 1920x40+0+0 \
-    -F "#ffffff" -B "#292d3e" -U "#ff0000" -u 3 \
-    -f "Ubuntu Mono Nerd Font"-18
+tail -f ~/.config/lemonbar/lemonpipe | lemonbar -p \
+    -g 1920x30+0+0 \
+    -F "#ffffff" -B "#292d3e" -U "#aa00ff" -u 3 \
+    -f "Ubuntu Mono Nerd Font"-17 -o -2 \
+    -f "material\-wifi"-16
